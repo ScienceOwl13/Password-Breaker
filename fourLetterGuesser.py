21576bd02b667e54120afa31e976b92f2c333600
ScienceOwl13 <groencg@gmail.com>
2022-03-09 11:58:08 -0500

    Password Guess Updated

--- fourLetterGuesser.py
+++ fourLetterGuesser.py
@@ -1,9 +1,42 @@
 import random
 import string
+import time
 
-string.ascii_letters
-'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+customPassword = input("Would you like to chose a password? y/n: ")
 
-number = random.choice(string.ascii_letters)
+if customPassword == "y":
+    password = input("Please input a four digit, lowercase password: ")
+    startTime = time.time()
+else:
+    number1 = random.choice(string.ascii_lowercase)
+    number2 = random.choice(string.ascii_lowercase)
+    number3 = random.choice(string.ascii_lowercase)
+    number4 = random.choice(string.ascii_lowercase)
+    password = f"{number1}{number2}{number3}{number4}"
+    startTime = time.time()
 
-print(number)
+print(f"The password is {password}")
+
+digit1 = "a"
+digit2 = "a"
+digit3 = "a"
+digit4 = "a"
+guess = f"{digit1}{digit2}{digit3}{digit4}"
+
+trys= 1
+
+while password != guess:
+    digit1 = random.choice(string.ascii_lowercase)
+    digit2 = random.choice(string.ascii_lowercase)
+    digit3 = random.choice(string.ascii_lowercase)
+    digit4 = random.choice(string.ascii_lowercase)
+    guess = f"{digit1}{digit2}{digit3}{digit4}"
+    print(guess, end="\r")
+    trys += 1
+
+endTime = time.time()
+
+totalTime = int(endTime) - int(startTime)
+
+print("\nThe password has been succesfully guessed. It took", trys, "trys and",totalTime, "seconds to guess the password.")
+print("That means the computer tested", int(trys/totalTime), "passwords per second!")
