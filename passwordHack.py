7d1e3207e9993258c3a35b48c5a8c6eb4ceb232d
ScienceOwl13 <groencg@gmail.com>
2022-03-12 16:58:16 -0500

    Commenting the code and formatting numbers to two decimal places!

--- passwordHack.py
+++ passwordHack.py
@@ -1,9 +1,10 @@
+# Import libraries
 import string
 import random
 import time
 
-characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
-guessCount = 0
+# List of characters that can be in the password
+characters = list(string.ascii_letters + string.digits + string.punctuation)
 
 # Password Generator
 def passwordGen():
@@ -12,12 +13,15 @@ def passwordGen():
     
     passLength = int(input("How many characters would you like the passcode to be: "))
     
+    # Rearrange the letters from the possible character list
     random.shuffle(characters)
 
+    # Create the password at the correct length
     password = []
     for i in range(passLength):
         password.append(random.choice(characters))
 
+    # Rearrange the combination of letters from the password
     random.shuffle(password)
 
     print("The password is:", "".join(password))
@@ -28,38 +32,67 @@ def passwordGuess():
 
     global guess
 
+    # Rearrange the list of characters
     random.shuffle(characters)
+
+    # Create the guess at the correct length
     guess = []
     for i in range(passLength):
         guess.append(random.choice(characters))
     
+    #Rearrange the list of shuffled characters
     random.shuffle(guess)
 
     print("".join(guess), end="\r")
 
 passwordGen()
 
+#This needs to be here for some reason, doesn't work otherwise
 guess = str
-guess2 = str
 
-input("Are you ready to have the computer find your password? ")
+#If you respond with "caterpillar", it will add an extra three seconds to the timer
+wait = input("Are you ready to have the computer find your password? ")
 
+# Mark the time that the guessing starts at
 startTime = time.time()
 
+# Keeps track of the amount of guesses
+guessCount = 0
 
+# Keep making a new guess until it is equal to the password
 while guess != password:
     passwordGuess()
     guessCount += 1
 
-# Calculate the time it takes (s)
+# Setting for testing the decimal places
+if wait == "caterpillar":
+    time.sleep(3)
+
+# Mark the time that the guessing ends at
 endTime = time.time()
-totalTime = int(endTime) - int(startTime)
 
+# Subtract the time that the guessing ends at from the time it starts at
+totalTime = endTime - startTime
+# Puts the formatted time into a two decimal places
+formattedTotalTime = "{:.2f}".format(totalTime)
+print(formattedTotalTime)
+
+# Gets the minutes out of the total seconds
 minutes = totalTime / 60
-
+# Gets the extra seconds from the minutes
 seconds = totalTime % 60
 
 print("The password has been guessed")
-print(totalTime, "totalTime")
-print(f"Test {int(minutes)} minutes, {int(seconds)} seconds.")
-print(guessCount, "guesses. That is", guessCount / totalTime, "guesses per second!")
+print(formattedTotalTime, "totalTime")
+print(f"Test {int(minutes)} minutes, {int(seconds)} seconds.")\
+
+# Fixes issue when it is instant, the script would fail as the seconds would be 0, and division by 0 is inpossible
+if formattedTotalTime == 0:
+    print("It only took the computer", guessCount, "guesses in", totalTime, "seconds to guess the passcode! That's crazy! Just imagine if that is a four digit passcode!")
+else:
+    #Formats the guess per second to two decimal places to be consistant with other numbers
+    print(guessCount, "guesses. That is", "{:.2f}".format(guessCount / totalTime), "guesses per second!")
+
+
+
+    
